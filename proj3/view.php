7a04cdd7c2dd8e0ec722f45d4fe86ebd883205ca
MOSES-do <68383557+MOSES-do@users.noreply.github.com>
2021-03-18 00:41:08 +0100

    Update view.php

--- proj3/view.php
+++ proj3/view.php
@@ -66,14 +66,14 @@ body{
 
             <form action="logout.php" method ="POST">
         
-             <input type = "submit"  value="Logout" style="margin-left:68em; position:absolute; top:0em;" name="logout" class="btn btn-success logout">
+             <input type = "submit"  value="Logout" style="margin-left:68em; position:absolute; top:2em;" name="logout" class="btn btn-success logout">
                   
              </form> 
 
         <?php 
             if(isset($_SESSION['Username'])){//This forces the page to redirect back to login.php if session is not set and if session is 
                 //set and open on multiple pages once one is logged out all is logged out.
-            echo "<span class='name'  style='margin:0px; font-weight:bold; position:absolute; width:8em; margin-left:74em; top:2px;'>" .'Hello, ' . $_SESSION["Username"] . "</span>";
+            echo "<span class='name'  style='margin:0px; font-weight:bold; position:absolute; width:8em; margin-left:74em; top:5px;'>" .'Hello, ' . $_SESSION["Username"] . "</span>";
             echo '<div class="dboard"><h1 style="text-align:center; margin-left:10em;">Welcome To Your Dashboard</h1></div>';
             } else {
                 echo "<script> location.href='login.php' </script>";
@@ -131,13 +131,13 @@ body{
 
                     <!-- Edit button -->
                     <td> <a href="<?php "form.php" ;?>updatepost.php?id=<?php echo $row['id']; ?>"
-                      class = "btn badge-info">Edit</a>
+                      class = "glyphicon glyphicon-info-sign">Edit</a>
                     </td>
 
                     <td>
                     <!-- Delete button -->
                      <a  href="<?php "form.php";?>deletepost.php?id=<?php echo $row['id']; ?>"  
-                    class = "btn badge-danger">Delete</a>   
+                    class = "glyphicon glyphicon-trash">Delete</a>   
                     </td> 
                 </tr>
                 <?php endwhile; ?>
